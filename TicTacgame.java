e45401fe577e8f89d8c4e5d49cbceb665d1c717a
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-07-26 18:02:49 +0530

    Added :- displaying placesin array

--- TicTacgame.java
+++ TicTacgame.java
@@ -1,31 +1,36 @@
 package com.bridgelabz;
 
-
+import java.util.Scanner;
 
 public class TicTacgame {
-	 ////assign a Char 1D array as a static
-    static char[] board = new char[10];
-    
+	 static Scanner input = new Scanner(System.in);
+	    public static void main(String[] args) {
+	        char[] board = new char[10];
 
-     /**(can Calling from Main method)  
- 		*     assign a empty space char by using 1D array
-      */
-    void CreateBoard() {
-        for (int i = 1; i < board.length; i++) {
-        	board[i]= ' ';
-        }
-        
-    }
-    //display Tic Tac Toe 1D array Box
-    //(can Calling from Main method)
-     
-	
-	public static void main(String[] args) {
-		System.out.println("*******WELCOME TO THE GAME**********");
-		   TicTacgame t = new TicTacgame();
-	        t.CreateBoard();
-	        
+	        System.out.println("Game initiated!");
 
-	
+	        //Display the initial board
+	        for(int i = 0; i < board.length; i++) {
+	            board[i] = '-';
+	        }
+	        showboard(board);
+
+	        char inputChoice = chooseLetter();
+	        System.out.println("Player has chosen " +inputChoice);
+
+	    }
+
+	    public static void showboard(char[] board) {
+	        System.out.println("\n" + "Displaying the Tic Tac Toe Board");
+	        System.out.println(board[0] + "  " + board[1] + "  " + board[2]);
+	        System.out.println(board[3] + "  " + board[4] + "  " + board[5]);
+	        System.out.println(board[6] + "  " + board[7] + "  " + board[8]);
+	    }
+
+	    public static char chooseLetter() {
+	        System.out.println("\n" + "Enter your choice: X | O");
+	        char choice = input.next().charAt(0);
+
+	        return choice;
 	    }
 	}
